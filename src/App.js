9f02796bf937d0e1ec87df29b3defd23939d1403
greedygoat000 <113379800+greedygoat000@users.noreply.github.com>
2022-11-22 01:22:02 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
     {name: "Strawberries", type: "Berry", color: "Red", price: 3.50, "image": "images/strawberry.PNG"},
     {name: "Blueberries", type: "Berry", color: "Blue", price: 2.28, "image": "images/blueberry.PNG"},
     {name: "Raspberries", type: "Berry", color: "Red", price: 3.56, "image": "images/raspberry.PNG"},
-    {name: "Marionberries", type: "Berry", color: "Black", price: 5.32, "image": "marion.png"},
-    {name: "Salmonberries", type: "Berry", color: "Orange", price: 4.50, "image": "salmon.png"},
-    {name: "Cherries", type: "Berry", color: "Red", price: 3.99, "image": "images/cherry.png"},
+    {name: "Marionberries", type: "Berry", color: "Black", price: 5.32, "image": "images/marion.PNG"},
+    {name: "Salmonberries", type: "Berry", color: "Orange", price: 4.50, "image": "images/salmon.PNG"},
+    {name: "Cherries", type: "Berry", color: "Red", price: 3.99, "image": "images/cherry.PNG"},
 
     {name: "Peach", type: "Tree Fruit", color: "Orange", price: 3.00, "image": "images/peach.png"},
     {name: "Plum", type: "Tree Fruit", color: "Purple", price: 1.20, "image": "images/plum.png"},
